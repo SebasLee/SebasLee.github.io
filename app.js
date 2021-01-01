940936ed62f0f6020e35b6c1f16a88bf0801148b
SebasLee <heechanlee98@gmail.com>
2021-01-01 15:50:23 -0800

    project section update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("bio-btn");
+var btn = document.getElementById("contact-btn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
